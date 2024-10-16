31710a4930a6511efa17eb6f6be7f62f15ad49ad
mohsen <mohsenmsmdev@gmail.com>
2024-10-16 12:39:01 +0330

    fea: add url equivalents

---
+++ app/constants/urlEquivalents.ts
@@ -0,0 +1,12 @@
+export const UrlEquivalent = [
+    {
+        link: "service/resturant",
+        eng_link: "resturant",
+        persian_link: "رستوران",
+    },
+    {
+        link: "service/coffe",
+        eng_link: "coffe",
+        persian_link: "کافه",
+    },
+];
