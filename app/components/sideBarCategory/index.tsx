20310d00a24d8daa32d49c6d2ad411c77da408a5
mohsen <mohsenmsmdev@gmail.com>
2024-10-16 17:15:09 +0330

    feat: adds sidebarCategory

---
+++ app/components/sideBarCategory/index.tsx
@@ -0,0 +1,9 @@
+export default function SideBarCategory() {
+    return (
+        <div className="rounded-lg shadow-sm col-span-1 p-4 border border-gray-200">
+            <div className="bg-gray-200 rounded-md p-3">
+                <span className="text-base font-bold text-gray-700">همه دسته‌بندی‌ها</span>
+            </div>
+        </div>
+    );
+}
