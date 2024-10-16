b5de45533d77d9f42820e8f15e3b8adc67c66d94
mohsen <mohsenmsmdev@gmail.com>
2024-10-16 16:41:34 +0330

    feat: update service [id] page

--- app/service/[id]/page.tsx
+++ app/service/[id]/page.tsx
@@ -1,3 +1,16 @@
+import BreadCrumb from "@/app/components/breadCrumb";
+import CategorySlider from "@/app/components/categorySlider";
+import SideBarCategory from "@/app/components/sideBarCategory";
+
 export default function page({ params }: { params: { id: string } }) {
-    return <main></main>;
+    return (
+        <main className="mx-5">
+            <CategorySlider />
+            <BreadCrumb />
+            <div className="grid grid-cols-4 gap-10 my-8">
+                <SideBarCategory />
+                <div className="col-span-3 bg-red-400 grid grid-cols-3 gap-10"></div>
+            </div>
+        </main>
+    );
 }
