dc5e0a3d3c713187b288f581d0cb39a6a1d9cae1
mohsen <mohsenmsmdev@gmail.com>
2024-10-22 11:53:22 +0330

    feat: adds dialog to sidebar menu

--- app/service/menu/components/sideBarMenu.tsx
+++ app/service/menu/components/sideBarMenu.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import pic from "@/app/images/TempPics/fitfriends.jpg";
-import { Star } from "lucide-react";
+import { CircleAlert, Star } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import ProviderInformation from "@/app/components/providerInformation";
+
 export default function SideBarMenu() {
     return (
-        <div className="w-full col-span-1 flex justify-start items-start flex-col gap-4">
-            <div className="h-fit flex justify-start items-center gap-4">
+        <div className="sticky top-[100px] w-full h-fit col-span-1 flex justify-start items-start flex-col gap-4">
+            <div className="h-[80px] flex justify-start items-center gap-4">
                 <div className="w-[80px] h-[80px] p-1 bg-white shadow-md shadow-gray-200 rounded-lg">
                     <Image
                         src={pic}
@@ -15,11 +19,28 @@ export default function SideBarMenu() {
                 <div className="h-full py-2 flex flex-col justify-between items-start">
                     <div className="flex justify-center items-center gap-1">
                         <Star size={16} className="text-yellow-500" />
-                        <span className="text-sm flex justify-center items-center gap-2">4.5 <span className="text-gray-400">(۳۴,۸۴۳ امتیاز)</span></span>
+                        <span className="text-sm flex justify-center items-center gap-2">
+                            4.5{" "}
+                            <span className="text-gray-400">
+                                (۳۴,۸۴۳ امتیاز)
+                            </span>
+                        </span>
                     </div>
-                    <span className="text-xl font-bold">فیت فرندز</span>
+                    <span className="text-2xl font-bold">فیت فرندز</span>
                 </div>
             </div>
+
+            <Dialog>
+                <DialogTrigger className="w-full">
+                    <div className="w-full py-3 bg-white hover:bg-white shadow-sm rounded-full flex justify-center items-center gap-1 text-green-500 hover:text-green-500 text-base">
+                        <CircleAlert size={18} />
+                        اطلاعات و نظرات
+                    </div>
+                </DialogTrigger>
+                <DialogContent>
+                    <ProviderInformation />
+                </DialogContent>
+            </Dialog>
         </div>
     );
 }
