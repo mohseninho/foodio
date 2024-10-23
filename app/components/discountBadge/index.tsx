68b7cde0ff849db38208ef0931804d33d3a5848e
mohsen <mohsenmsmdev@gmail.com>
2024-10-23 13:04:35 +0330

    feat: adds discount badge

---
+++ app/components/discountBadge/index.tsx
@@ -0,0 +1,9 @@
+export default function DiscountBadge() {
+    return (
+        <div className="w-fit h-fit rounded-lg bg-pink-300 text-pink-500 p-1 text-base flex justify-center items-center">
+            <span>
+                <span>20</span>%
+            </span>
+        </div>
+    );
+}
